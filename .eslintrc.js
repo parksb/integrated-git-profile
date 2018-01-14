135704f6c9d64a164d4e29c7029777d31579f3b8
ParkSB <parkgds@gmail.com>
2018-01-15 01:43:51 +0900

    Change eslint config

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,31 @@
 module.exports = {
-    "extends": "standard",
-    "rules": {
-        semi: [2, "always"]
-    },
+  extends: 'standard',
+  rules: {
+    'semi': [2, 'always'],
+    'no-unused-vars': 'warn',
+    'camelcase': ['error', { properties: 'never' }],
+    'comma-spacing': ['error', { before: false, after: true }],
+    indent: ['error', 2, {
+      SwitchCase: 1,
+      VariableDeclarator: 1,
+      outerIIFEBody: 1,
+      FunctionDeclaration: {
+        parameters: 1,
+        body: 1
+      },
+      FunctionExpression: {
+        parameters: 1,
+        body: 1
+      },
+      CallExpression: {
+        arguments: 1
+      },
+      ArrayExpression: 1,
+      ObjectExpression: 1,
+      ImportDeclaration: 1,
+      flatTernaryExpressions: false,
+      ignoredNodes: ['JSXElement', 'JSXElement > *', 'JSXAttribute', 'JSXIdentifier', 'JSXNamespacedName', 'JSXMemberExpression', 'JSXSpreadAttribute', 'JSXExpressionContainer', 'JSXOpeningElement', 'JSXClosingElement', 'JSXText', 'JSXEmptyExpression', 'JSXSpreadChild'],
+      ignoreComments: false
+    }],
+  },
 }
